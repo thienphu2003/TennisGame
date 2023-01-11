41af17d8ac45de75711a6eb414a509c2d5efc563
Your Name <you@example.com>
2023-01-11 14:09:03 +0700

    Tennis Game

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(TennisGame.getScore("Phu","Trung",30,0));
-
-    }
-}
--- TennisGame.java
+++ TennisGame.java
@@ -1,69 +1,65 @@
 public class TennisGame {
 
-    public static final String SCORE = "";
-    public static final int TEMP_SCORE = 0;
-    
+    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+        String score = "";
+        int tempScore=0;
+        if (m_score1==m_score2)
+        {
+            switch (m_score1)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                case 3:
+                    score = "Forty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
 
-    public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
-        String score = SCORE;
-        int tempScore= TEMP_SCORE;
-        if (player1Score==player2Score)
-        {
-            score= getEqualScore(player1Score);
+            }
         }
-        else if (player1Score>=4 || player2Score>=4)
+        else if (m_score1>=4 || m_score2>=4)
         {
-            score = getAdvantageOrWinScore(player1Score, player2Score);
+            int minusResult = m_score1-m_score2;
+            if (minusResult==1) score ="Advantage player1";
+            else if (minusResult ==-1) score ="Advantage player2";
+            else if (minusResult>=2) score = "Win for player1";
+            else score ="Win for player2";
         }
         else
         {
-            score = getNormalScore(player1Score, player2Score, score);
-        }
-        return score;
-    }
-
-    private static String getNormalScore(int player1Score, int player2Score, String score) {
-        int tempScore;
-        for (int i = 1; i<3; i++)
-        {
-            if (i==1)
+            for (int i=1; i<3; i++)
             {
-                tempScore = player1Score;
-            }
-            else
-            {
-                score +="-";
-                tempScore = player2Score;
-            }
-            switch (tempScore) {
-                case 0 -> score += "Love";
-                case 1 -> score += "Fifteen";
-                case 2 -> score += "Thirty";
-                case 3 -> score += "Forty";
+                if (i==1) tempScore = m_score1;
+                else { score+="-"; tempScore = m_score2;}
+                switch(tempScore)
+                {
+                    case 0:
+                        score+="Love";
+                        break;
+                    case 1:
+                        score+="Fifteen";
+                        break;
+                    case 2:
+                        score+="Thirty";
+                        break;
+                    case 3:
+                        score+="Forty";
+                        break;
+                }
             }
         }
         return score;
     }
-
-    private static String getAdvantageOrWinScore(int player1Score, int player2Score) {
-        String score;
-        int minusResult = player1Score - player2Score;
-        if (minusResult==1) score ="Advantage player1";
-        else if (minusResult ==-1) score ="Advantage player2";
-        else if (minusResult>=2) score = "Win for player1";
-        else score ="Win for player2";
-        return score;
-    }
-
-    private static String getEqualScore(int player1Score) {
-        String score;
-        score = switch (player1Score) {
-            case 0 -> "Love-All";
-            case 1 -> "Fifteen-All";
-            case 2 -> "Thirty-All";
-            case 3 -> "Forty-All";
-            default -> "Deuce";
-        };
-        return score;
+    public static void main (String[] args)
+    {
+        System.out.println(TennisGame.getScore("Trung","Phu",30,15));
     }
 }
